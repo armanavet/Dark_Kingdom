225f9392a9e9845246fd835a7b689977f4c2ab3f
Acv-Try <arkadi.ghasumyanyw@gmail.com>
2025-04-03 21:00:12 +0400

    Create random tile generation algorithm

---
+++ Assets/_Game/Scripts/Tile.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    GameTileContentType Type;
+
+    [SerializeField] GameObject NeutralTile, OwnTile, ObstructedTile;
+
+    private void Start()
+    {
+
+        //NeutralTile.SetActive(false);
+        //OwnTile.SetActive(false);
+        //ObstructedTile.SetActive(false);
+    }
+
+
+    public void SetType(GameTileContentType type)
+    {
+        Type = type;
+        NeutralTile.SetActive(false);
+        OwnTile.SetActive(false);
+        ObstructedTile.SetActive(false);
+        switch (type)
+        {
+            
+            case GameTileContentType.Neutral:
+                NeutralTile.SetActive(true);
+                break;
+            case GameTileContentType.Own:
+                OwnTile.SetActive(true);
+                break;
+            case GameTileContentType.Obstructed:
+                ObstructedTile.SetActive(true);
+                break;
+
+            default: break;
+        }
+
+        
+    }
+}
---
+++ Assets/_Game/Scripts/TileGenerator.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.WSA;
+
+public class TileGenerator : MonoBehaviour
+{
+    [SerializeField] Vector2Int _mapSize;
+    [SerializeField] Tile _tilePrefab;
+    [SerializeField] float _scale;
+    [SerializeField] bool PrimeryVersion;
+
+
+    private void Start()
+    {
+        if (PrimeryVersion)
+        {
+            Initialize1();
+        }
+        else
+        {
+            Initialize2();
+        }
+    }
+
+    void Initialize1()
+    {
+        float primeryNoise = 0;
+        Vector2 offset = new Vector2((_mapSize.x - 1) * 0.5f, (_mapSize.y - 1) * 0.5f);
+        for (int y = 0; y < _mapSize.y; y++)
+        {
+            for (int x = 0; x < _mapSize.x; x++)
+            {
+                Tile tile = Instantiate(_tilePrefab, transform);
+                tile.transform.localPosition = new Vector3(x - offset.x, 0, y - offset.y);
+                primeryNoise = Mathf.PerlinNoise(x* _scale, y* _scale);
+                if (primeryNoise <= 0.15f)
+                {
+                    tile.SetType(GameTileContentType.Own);
+                } else if (primeryNoise > 0.15f && primeryNoise < 0.6f)
+                {
+                    tile.SetType(GameTileContentType.Obstructed);
+                }else
+                {
+                    tile.SetType(GameTileContentType.Neutral);
+                }
+            }
+        }
+
+    }
+
+    void Initialize2()
+    {
+        float primeryNoise = 0;
+        float secondaryNoise = 0;
+        Vector2 offset = new Vector2((_mapSize.x - 1) * 0.5f, (_mapSize.y - 1) * 0.5f);
+        for (int y = 0; y < _mapSize.y; y++)
+        {
+            for (int x = 0; x < _mapSize.x; x++)
+            {
+                Tile tile = Instantiate(_tilePrefab, transform);
+                tile.transform.localPosition = new Vector3(x - offset.x, 0, y - offset.y);
+                primeryNoise = Mathf.PerlinNoise(x * _scale, y * _scale);
+                tile.SetType(primeryNoise < 0.5f ? GameTileContentType.Neutral : GameTileContentType.Obstructed);
+                secondaryNoise = Mathf.PerlinNoise(x * _scale * 0.5f, y * _scale * 0.5f);
+                if (secondaryNoise < 0.2f)
+                {
+                    tile.SetType(GameTileContentType.Own);
+                }
+            }
+        }
+
+    }
+
+
+}
+public enum GameTileContentType
+{
+    Neutral,
+    Own,
+    Obstructed
+}
