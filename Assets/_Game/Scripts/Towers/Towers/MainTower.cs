b879032e2abfa4b8a568f41d0ed7cc2969fa3324
Acv-Try <arkadi.ghasumyanyw@gmail.com>
2025-09-08 21:28:33 +0400

    Follow changes
    Add main towers shooting system, but with some issues.

--- Assets/_Game/Scripts/Towers/Towers/MainTower.cs
+++ Assets/_Game/Scripts/Towers/Towers/MainTower.cs
@@ -1,22 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.Rendering;
 using static UnityEngine.GraphicsBuffer;
 
 [System.Serializable]
-class MainTowerDefender 
+public class MainTowerDefender
 {
-    Transform tower;
-    [HideInInspector] Enemy target;
-    [HideInInspector] float attackSpeed;
+    public Transform turret;
+    [HideInInspector] public Enemy target;
+    [HideInInspector] public float cooldown;
+}
+
+[System.Serializable]
+public class TowersRootPointPositions
+{
+    public float[] x;
+    public float[] y;
+
 }
 public class MainTower : Tower
 {
     [SerializeField] List<int> GoldGenerationList;
-    List<MainTowerDefender> mianTowerDefender;
-    //[SerializeField, Range(1, 10f)]
-    //float attackRange = 2f;
+    [SerializeField] private List<MainTowerDefender> Defender;
+    [SerializeField] List<float> ShootingPointPositions;
+    [SerializeField] List<Transform> TowersRootPoints;
+    [SerializeField] private TowersRootPointPositions[] TowersRootPointPositions;
+    [SerializeField] float ProjectileSpeed;
+    [SerializeField] float AttackSpeed;
+    [SerializeField, Range(1, 10f)]
+    float attackRange = 2f;
     float damage;
 
     private void Start()
@@ -28,10 +43,32 @@ public class MainTower : Tower
         damage = Damage[CurrentLevel];
         maxHP = HP[CurrentLevel];
         model = Models[CurrentLevel];
-
+        projectile = Projectiles[CurrentLevel];
         currentHP = currentHP == 0 ? maxHP : currentHP;
+        CountTowerPointPositionsOnEachLevel(CurrentLevel, TowersRootPoints, TowersRootPointPositions);
+        foreach (var defender in Defender)
+        {
+            defender.turret.position = new Vector3(defender.turret.position.x, ShootingPointPositions[CurrentLevel], defender.turret.position.z);
+            defender.cooldown = 1 / AttackSpeed;
+            Debug.Log(defender.cooldown);
+        }
     }
 
+    private void Update()
+    {
+        foreach (var defender in Defender)
+        {
+            if (defender.cooldown <= 0)
+            {
+                if (AcquireTarget(defender))
+                {
+                    Shoot(defender);
+                }
+                defender.cooldown = Random.Range(0.1f, 1f / AttackSpeed);
+            }
+            defender.cooldown -= Time.deltaTime;
+        }
+    }
     public override void Upgrade()
     {
         if (CurrentLevel < UpgradePrices.Count)
@@ -39,55 +76,93 @@ public class MainTower : Tower
             UpgradePrice = UpgradePrices[CurrentLevel];
             EconomyManager.Instance.ChangeGoldAmount(-UpgradePrice);
 
-            CurrentLevel++; 
+            CurrentLevel++;
 
             damage = Damage[CurrentLevel];
             GoldGenerated = GoldGenerationList[CurrentLevel];
 
             model.SetActive(false);
             model = Models[CurrentLevel];
+            CountTowerPointPositionsOnEachLevel(CurrentLevel, TowersRootPoints, TowersRootPointPositions);
+            foreach (var defender in Defender)
+            {
+                defender.turret.position = new Vector3(defender.turret.position.x, ShootingPointPositions[CurrentLevel], defender.turret.position.z);
+            }
             model.SetActive(true);
 
             float hpPercent = currentHP / maxHP;
             maxHP = HP[CurrentLevel];
             currentHP = maxHP * hpPercent;
         }
+
+
     }
 
-    //bool AcquireTarget()
-    //{
-    //    Collider[] targets;
-    //    targets = Physics.OverlapSphere(transform.position, attackRange, illusionMask);
-    //    if (targets.Length == 0)
-    //    {
-    //        targets = Physics.OverlapSphere(transform.position, attackRange, enemyMask);
-    //    }
-    //    if (targets.Length > 0)
-    //    {
-    //        int ClosestTargetIndex = 0;
-    //        float MinDist = Vector3.Distance(transform.position, targets[ClosestTargetIndex].transform.position);
-    //        for (int i = 1; i < targets.Length; i++)
-    //        {
-    //            if (MinDist <= MinDist + i)
-    //            {
-    //                float dist = Vector3.Distance(transform.position, targets[i].transform.position);
-    //                if (dist < MinDist)
-    //                {
-    //                    MinDist = dist;
-    //                    ClosestTargetIndex = i;
-    //                }
-    //            }
+    void Shoot(MainTowerDefender defender)
+    {
+        Vector3 point = defender.target.transform.position;
+        float travelDistance = Vector3.Distance(defender.turret.position, point);
+        float travelTime = travelDistance / ProjectileSpeed;
+        GameObject newProjectile = Instantiate(projectile, defender.turret.position, Quaternion.LookRotation(point - defender.turret.position));
+        newProjectile.GetComponent<Arrow>()?.Initialize(ProjectileSpeed);
+        StartCoroutine(HitTarget(defender, newProjectile, travelTime));
+    }
 
-    //        }
-    //        target = targets[ClosestTargetIndex].GetComponent<Enemy>();
-    //        if (target != null)
-    //        {
-    //            return true;
-    //        }
-    //        else
-    //            return false;
-    //    }
-    //    target = null;
-    //    return false;
-    //}
+    bool AcquireTarget(MainTowerDefender defender)
+    {
+        Collider[] PotentialTargets = Physics.OverlapSphere(transform.position, attackRange, illusionMask);
+        if (PotentialTargets.Length == 0) PotentialTargets = Physics.OverlapSphere(transform.position, attackRange, enemyMask);
+        if (PotentialTargets.Length > 0)
+        {
+            if (defender.target == null)
+            {
+                Collider closestTarget = PotentialTargets[0];
+                float minDistance = Vector3.Distance(defender.turret.position, closestTarget.transform.position);
+                foreach (var potentialTargets in PotentialTargets)
+                {
+                    float distance = Vector3.Distance(defender.turret.position, potentialTargets.transform.position);
+                    if (distance < minDistance)
+                    {
+                        minDistance = distance;
+                        closestTarget = potentialTargets;
+                    }
+                }
+                if (closestTarget != null)
+                {
+                    defender.target = closestTarget.GetComponent<Enemy>();
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        defender.target = null;
+        return false;
+
+    }
+
+    void CountTowerPointPositionsOnEachLevel(int currentLevel, List<Transform> towersRootPoints, TowersRootPointPositions[] towersRootPointPositions)
+    {
+        for (int j = 0; j < towersRootPoints.Count; j++)
+        {
+            towersRootPoints[j].position = new Vector3(towersRootPointPositions[currentLevel].x[j], towersRootPoints[j].position.y, towersRootPointPositions[currentLevel].y[j]);
+        }
+    }
+    IEnumerator HitTarget(MainTowerDefender defender, GameObject currentProjectile, float arriveTime)
+    {
+        yield return new WaitForSeconds(arriveTime);
+
+        if (defender.target != null)
+        {
+            defender.target.ApplyDamage(damage);
+            //foreach (var debuff in currentDebuffs)
+            //{
+            //    DebuffManager.Instance.ApplyDebuff(defender.target, debuff);
+            //}
+        }
+
+        Destroy(currentProjectile);
+    }
 }
