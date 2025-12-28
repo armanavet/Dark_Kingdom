75066a09da622e6de7bbfec11744e5df33756dd7
Acv-Try <arkadi.ghasumyanyw@gmail.com>
2025-12-28 16:20:31 +0400

    Bug fix
    Fixed type mismatches in the 'WaveManager'.

--- Assets/_Game/Scripts/Managers/StateManager.cs
+++ Assets/_Game/Scripts/Managers/StateManager.cs
@@ -14,7 +14,7 @@ public class StateManager : MonoBehaviour, ISaveable
     GameState PreviusState;
     float Timer;
     public int timeMultiplier = 1;
-    int currentWave = 9;
+    int currentWave = 0;
 
     #region Singleton
     private static StateManager _instance;
