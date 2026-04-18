ee6dc23867f125235bf0fafce4d88f26f7cbab2b
hayk-hovyan <hayk.hovyan.1@gmail.com>
2026-04-18 21:16:03 +0400

    Fix incorrect color format when generating tower icons

--- Assets/_Game/Scripts/UI/IconFromObject.cs
+++ Assets/_Game/Scripts/UI/IconFromObject.cs
@@ -19,25 +19,25 @@ public class IconFromObject : MonoBehaviour
 
     private void CaptureObject()
     {
-        Texture2D texture = new Texture2D(rt.width, rt.height, TextureFormat.RGBA32, false, true);
+        Texture2D texture = new Texture2D(rt.width, rt.height, TextureFormat.RGBA32, false, false);
 
         var oldGL = GL.sRGBWrite;
         var oldRt = RenderTexture.active;
-        GL.sRGBWrite = false;
         RenderTexture.active = rt;
 
         texture.ReadPixels(new Rect(0, 0, rt.width, rt.height), 0, 0);
         texture.Apply();
 
-        GL.sRGBWrite = oldGL;
         RenderTexture.active = oldRt;
         Directory.CreateDirectory(iconSavePath);
 
-        string fileName = $"Capture_{DateTime.Now.ToString("yyyy_MM_dd_HH_mm_ss")}.png";
+        int counter = 0;
+        string fileName = $"Capture_{counter}.png";
         string savePath = Path.Combine(iconSavePath, fileName);
         while (File.Exists(savePath))
         {
-            fileName = $"Capture_{DateTime.Now.ToString("yyyy_MM_dd_HH_mm_ss")}" + "(1).png";
+            counter++;
+            fileName = $"Capture_{counter}.png";
             savePath = Path.Combine(iconSavePath, fileName);
         }
 
